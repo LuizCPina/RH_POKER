95966b55deac7c2460eac2d060d2b7db6c77ff94
Luiz Henrique <luizricopina@hotmail.com>
2026-03-19 15:43:24 -0300

    add whitenoise

--- stepP/settings.py
+++ stepP/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -123,4 +126,4 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = 'lista_vagas'
 LOGOUT_REDIRECT_URL = '/vagas/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
